97557e1bf1a262aadfb59d74081d7303af76d614
Tommy Chang <tommy.y.chang@gmail.com>
2022-10-16 16:17:20 -0400

    Backlog ref #3: Fix GitHub CI pull request behavior
    This commit removes the GitHub CI workflow error that was
    intentionally introduced in the last commit.
    
    Extra unused codes are added to see if Coveralls.io would report less
    than 100% code coverage.

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
 
+void function_not_used()
+{
+  int notUsed = 0;
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
